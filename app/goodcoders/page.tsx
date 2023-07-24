944a24584bb0307ac8f6e7fbd8a24d1468c01f2d
dc <00danzy@gmail.com>
2023-07-24 18:29:45 +0100

    improve styles

--- app/goodcoders/page.tsx
+++ app/goodcoders/page.tsx
@@ -1,23 +1,33 @@
 import Link from "next/link";
 
-const Details = () => {
+const getUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return await res.json();
+};
+
+type user = { id: number; name: string; username: string; email: string };
+
+const Details = async () => {
+  const users = await getUsers();
+
   return (
     <main className="main">
-      <h1 className='text-xl font-bold text-gray-900'>All Goodcoders</h1>
+      <h1 className="text-xl font-bold text-gray-900">All Goodcoders</h1>
 
       <div className="mt-7">
         {/* List of goodcoders */}
-        <div className="mt-3">
-          <Link href={`/details/${5}`}>
-            <div className="bg-white px-6 py-5 border-l-4 rounded-sm border-white hover:border-blue-500">
-              <p>Luke Shaw</p>
-            </div>
-          </Link>
-        </div>
-        
+        {users.map((user: user) => (
+          <div className="mt-3" key={user.id}>
+            <Link href={`/goodcoders/${user.id}`}>
+              <div className="bg-white px-6 py-5 border-l-4 rounded-sm border-white hover:border-blue-500">
+                <p>{user.name}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </main>
   );
-}
+};
 
 export default Details;
