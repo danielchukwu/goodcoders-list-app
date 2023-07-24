42ecb4486753cca56530cb4734b36590c2cd8ac9
dc <00danzy@gmail.com>
2023-07-24 14:35:20 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Footer, Navbar } from '@/components/index'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + ' bg-gray-100 text-gray-700'}>
+        <Navbar />
+
+        {children}
+
+        <Footer />
+        
+      </body>
     </html>
   )
 }
